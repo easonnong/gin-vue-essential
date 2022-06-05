27d71549a0ef0e14dfc62bbec2e5d5b8b70a3fe7
easonnong <nongeason@gmail.com>
2022-06-05 11:27:30 +0800

    配置vue

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/scss/index.scss';
+
+Vue.config.productionTip = false;
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
